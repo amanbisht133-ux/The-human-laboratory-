640a47e3973b2aa8ed4d86a6fea86167f13582ab
Abhishek Manral <manralabishek@gmail.com>
2026-04-23 18:30:36 +0530

    chore: update cali lab weekend timing to 8:30-10am

--- src/pages/CaliLab.tsx
+++ src/pages/CaliLab.tsx
@@ -91,7 +91,7 @@ export default function CaliLab() {
             'Morning': ['6:30 AM - 7:30 AM', '7:30 AM - 8:30 AM', '8:30 AM - 9:30 AM', '9:30 AM - 10:30 AM'],
             'Evening': ['6:00 PM - 7:00 PM', '7:00 PM - 8:00 PM', '8:00 PM - 9:00 PM', '9:00 PM - 10:00 PM']
         },
-        'Weekend': ['9:00 AM - 10:00 AM'],
+        'Weekend': ['8:30 AM - 10:00 AM'],
         'Kids Batch': {
             'Morning': ['9:00 AM - 10:00 AM', '10:00 AM - 11:00 AM'],
             'Evening': ['4:00 PM - 5:00 PM', '5:00 PM - 6:00 PM']
